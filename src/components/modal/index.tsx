d7fd712c575c7516ef69764657bd41bbab56304a
田若茜 <1040461147@qq.com>
2024-04-17 14:45:12 +0800

    ui: update

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -32,7 +32,7 @@ const Modal: FC<IProps> = memo(({ children, handleClose, title, subTitle }) => {
   }
 
   return (
-    <div className={`modal fixed inset-0 z-40`}>
+    <div className={`modal fixed inset-0 z-50`}>
       {/* Mask */}
       <div
         className={`modal-mask absolute inset-0 bg-black/50 transition-opacity duration-300 ${
@@ -43,17 +43,17 @@ const Modal: FC<IProps> = memo(({ children, handleClose, title, subTitle }) => {
 
       {/* Content */}
       <main
-        className={`modal-content absolute inset-x-0 px-5 py-7 pt-[--ssm-modal-pt] mt-[50vh] -translate-y-1/2 mx-auto w-full h-full xs:w-[475px] xs:h-auto xs:p-6 rounded-2xl bg-[--bg-dark-blue] text-gray-300 shadow-lg transition-all duration-300 ${
+        className={`modal-content overflow-y-auto absolute inset-x-0 px-5 py-7 pt-[--ssm-modal-pt] mt-[50vh] -translate-y-1/2 mx-auto w-full h-full xs:w-[475px] xs:h-auto xs:p-6 rounded-2xl bg-[--bg-dark-blue] text-gray-300 shadow-lg transition-all duration-300 ${
           opened ? 'opacity-100 xs:!scale-100' : 'opacity-0 xs:scale-75'
         }`}
       >
-        <header className="modal-content-header mb-5">
-          <div className="moal-content-header-title flex flex-col text-center">
+        <header className="modal-content-header flex justify-between mb-5">
+          <div className="moal-content-header-title flex flex-col flex-1">
             <span className="text-xl mb-1">{title}</span>
             {subTitle && <span className="text-sm text-gray-300/80">{subTitle}</span>}
           </div>
           <button
-            className="absolute top-3 right-3 hover:text-gray-300 focus:outline-none transition-colors p-1"
+            className=" hover:text-gray-300 focus:outline-none transition-colors p-1"
             onClick={handleModalClose}
           >
             <Image src="/images/common/close.svg" width={20} height={20} alt="exit" />
