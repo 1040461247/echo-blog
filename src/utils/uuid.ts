5881903fe6cab972333c3b4477dc05b3accdb7d1
田若茜 <1040461147@qq.com>
2024-05-19 16:17:00 +0800

    update

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,6 +1,14 @@
 function uuid() {
-  const uuid = window.crypto.getRandomValues(new Uint8Array(8))
-  return uuid.toString().split(',').join('')
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = window.crypto.getRandomValues(new Uint8Array(1))[0] % 16 | 0
+    const v = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
 }
+
+// function uuid() {
+//   const uuid = window.crypto.getRandomValues(new Uint8Array(8))
+//   return uuid.toString().split(',').join('')
+// }
 
 export default uuid
